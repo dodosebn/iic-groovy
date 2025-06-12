ea363431d1d9f49017854b299c5871130ba4e882
orji Dominion <orjidominion32@gmail.com>
2025-06-12 17:25:22 +0100

    added sResult

--- app/components/home/home.tsx
+++ app/components/home/home.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Intro from './intro'
+import SResult from './sResult';
 
 const Homee = () => {
   return (
     <div>
       <Intro />
+      <SResult />
     </div>
   )
 }
