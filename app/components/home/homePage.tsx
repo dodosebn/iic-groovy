2614cf45a8a5aabea9895a152fbd584ca1e6ebc1
orji Dominion <orjidominion32@gmail.com>
2025-06-15 21:15:38 +0100

    opss

--- app/components/home/homePage.tsx
+++ app/components/home/homePage.tsx
@@ -1,19 +1,18 @@
 // 'use client';
-import React from 'react'
+import React from 'react';
 import HomeWrapper from './customs/homeWrapper';
 import Page1Maps from './data/page1Maps';
 
-const HomePage = () => {
-  
+const HomePage = ({ num }: { num: number }) => {
   return (
     <div className="home-container">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-        {Page1Maps.slice(0, 6).map((item) => (
+        {Page1Maps.slice(0, num).map((item) => (
           <HomeWrapper key={item.id} {...item} />
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default HomePage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,68 @@
+'use client';
+
 import TransitionLink from "@/utils/transitionLink";
 import HomePage from "./components/home/homePage";
 import MeetAuthor from "./components/home/customs/meetAuthor";
-
-
-
+import { useState } from "react";
 
 export default function Home() {
+  const [subscribed, setSubscribed] = useState(false);
+
   return (
     <div className="flex justify-center flex-col px-[1rem]">
-     <HomePage/>
-     <div className="flex gap-3 w-full mt-6 justify-center">
-      <div>
-      <p>Page 1 of 2</p>
-      </div>
-      <div>
-        <TransitionLink href="/page2" > 
-          <button 
-      className='bg-[#ff4c60] border border-[#333] text-[rgb(0,0,0)] px-[1rem] py-[0.2rem] 
-      rounded-2xl flex items-center gap-2 
-  transition-transform duration-300 ease-in-out hover:shadow-[2px_2px_0px_0px_#000] hover:-translate-y-0.5'
-    >
-Next
-      </button>
-      </TransitionLink>
-      </div>
-     </div>
-                           <MeetAuthor />
+      <HomePage num={subscribed ? 6 : 3} />
 
+      {!subscribed && (
+        <p className="text-lg mt-2">
+          <span 
+            className="font-bold text-[#ff4c60] cursor-pointer hover:underline"
+            onClick={() => setSubscribed(true)}
+          >
+            Subscribe
+          </span>{" "}
+          to view more articles
+        </p>
+      )}
+
+      {/* Navigation */}
+      <div className="flex gap-3 w-full mt-6 justify-center">
+        <div>
+          <p>Page 1 of 2</p>
+        </div>
+        <div>
+          {subscribed ? (
+            <TransitionLink href="/page2">
+              <button 
+                className='bg-[#ff4c60] border border-[#333] text-[rgb(0,0,0)] px-[1rem] py-[0.2rem] 
+                           rounded-2xl flex items-center gap-2 
+                           transition-transform duration-300 ease-in-out hover:shadow-[2px_2px_0px_0px_#000] hover:-translate-y-0.5'
+              >
+                Next
+              </button>
+            </TransitionLink>
+          ) : (
+            <div className="relative group">
+              {/* Disabled Button */}
+              <button 
+                disabled
+                className='bg-gray-300 border border-[#333] text-gray-500 px-[1rem] py-[0.2rem] 
+                           rounded-2xl flex items-center gap-2 cursor-not-allowed'
+              >
+                Next
+              </button>
+
+              {/* Tooltip on hover */}
+              <div className="absolute bottom-full mb-2 left-1/2 -translate-x-1/2 
+                              bg-black text-white text-sm px-2 py-1 rounded opacity-0 
+                              group-hover:opacity-100 transition-opacity duration-200 pointer-events-none whitespace-nowrap z-10">
+                Subscribe to view more
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+
+      <MeetAuthor />
     </div>
   );
 }
