16dbae7149a2379d5493eaeafe88e0615348b1ac
orji Dominion <orjidominion32@gmail.com>
2025-06-23 22:46:51 +0100

    sacked

--- utils/bgDecor.tsx
+++ utils/bgDecor.tsx
@@ -1,27 +1,32 @@
 const BackgroundDecor = () => {
   return (
+    <>
+      <div className="fixed top-0 w-full h-full bg-[#54cbca] -z-10" />
+      
+      <div className="fixed top-32 w-full h-full inset-0 overflow-hidden -z-10">
+        {/* Left side - reduced by 2 Tailwind units (8px each) */}
+        <div className="absolute top-16 left-48 w-5 h-5 rounded-full bg-pink-300" /> {/* was left-52 */}
+        <div className="absolute top-16 left-64 w-16 h-2 bg-gray-200 rotate-[40deg]" /> {/* was left-72 */}
+        <div className="absolute top-52 left-48 w-5 h-5 rounded-full bg-pink-200" /> {/* was left-52 */}
+        <div className="absolute top-72 left-64 w-16 h-3 bg-gray-300 rotate-45" /> {/* was left-72 */}
+        <div className="absolute top-96 left-72 w-5 h-5 rounded-full bg-blue-200" /> {/* was left-80 */}
+        <div className="absolute top-[24rem] left-48 w-5 h-5 rounded-full bg-orange-400" /> {/* was left-52 */}
+        <div className="absolute top-[30rem] left-64 w-12 h-2 bg-pink-200 rotate-45" /> {/* was left-72 */}
+        <div className="absolute top-[36rem] left-72 w-5 h-5 rounded-full bg-blue-100" /> {/* was left-80 */}
+        <div className="absolute bottom-8 left-64 w-32 h-3 bg-orange-300 rotate-45" /> {/* was left-72 */}
 
-<div className="fixed top-0 w-full inset-0 bg-[#54cbca] overflow-hidden -z-10 ">
-      <div className="absolute top-16 left-12 w-5 h-5 rounded-full bg-pink-300" />
-      <div className="absolute top-16 left-28 w-16 h-2 bg-gray-200 rotate-[40deg]" />
-      <div className="absolute top-52 left-16 w-5 h-5 rounded-full bg-pink-200" />
-      <div className="absolute top-72 left-24 w-16 h-3 bg-gray-300 rotate-45" />
-      <div className="absolute top-96 left-28 w-5 h-5 rounded-full bg-blue-200" />
-      <div className="absolute top-[24rem] left-24 w-5 h-5 rounded-full bg-orange-400" />
-      <div className="absolute top-[30rem] left-28 w-12 h-2 bg-pink-200 rotate-45" />
-      <div className="absolute top-[36rem] left-32 w-5 h-5 rounded-full bg-blue-100" />
-      <div className="absolute bottom-8 left-28 w-16 h-3 bg-orange-300 rotate-45" />
-
-      <div className="absolute top-16 right-12 w-5 h-5 rounded-full bg-pink-300" />
-      <div className="absolute top-16 right-28 w-16 h-2 bg-gray-200 rotate-[40deg]" />
-      <div className="absolute top-52 right-16 w-5 h-5 rounded-full bg-pink-200" />
-      <div className="absolute top-72 right-24 w-16 h-3 bg-gray-300 rotate-45" />
-      <div className="absolute top-96 right-28 w-5 h-5 rounded-full bg-blue-200" />
-      <div className="absolute top-[24rem] right-24 w-5 h-5 rounded-full bg-orange-400" />
-      <div className="absolute top-[30rem] right-28 w-12 h-2 bg-pink-200 rotate-45" />
-      <div className="absolute top-[36rem] right-32 w-5 h-5 rounded-full bg-blue-100" />
-      <div className="absolute bottom-8 right-28 w-16 h-3 bg-orange-300 rotate-45" />
-    </div>
+        {/* Right side - matched to new left positions */}
+        <div className="absolute top-16 right-48 w-5 h-5 rounded-full bg-pink-300" /> {/* symmetric to left-48 */}
+        <div className="absolute top-16 right-64 w-16 h-2 bg-gray-200 rotate-[40deg]" /> {/* symmetric to left-64 */}
+        <div className="absolute top-52 right-48 w-5 h-5 rounded-full bg-pink-200" /> {/* symmetric to left-48 */}
+        <div className="absolute top-72 right-64 w-16 h-3 bg-gray-300 rotate-45" /> {/* symmetric to left-64 */}
+        <div className="absolute top-96 right-72 w-5 h-5 rounded-full bg-blue-200" /> {/* symmetric to left-72 */}
+        <div className="absolute top-[24rem] right-48 w-5 h-5 rounded-full bg-orange-400" /> {/* symmetric to left-48 */}
+        <div className="absolute top-[30rem] right-64 w-12 h-2 bg-pink-200 rotate-45" /> {/* symmetric to left-64 */}
+        <div className="absolute top-[36rem] right-72 w-5 h-5 rounded-full bg-blue-100" /> {/* symmetric to left-72 */}
+        <div className="absolute bottom-8 right-64 w-32 h-3 bg-orange-300 rotate-45" /> {/* symmetric to left-64 */}
+      </div>
+    </>
   );
 };
 
